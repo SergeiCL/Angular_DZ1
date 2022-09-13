f13c32fc0c4a4cc75f698e4e1747326bf4ebf377
Sergei Slepitsa <htcdesires121@gmail.com>
2022-09-13 23:37:06 +0300

    Add my card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app-dz1';
+
+  toggle = true;
+
+  toggleCards() {
+    this.toggle = !this.toggle
+
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {About_meComponent} from './about_me/about_me.component';
+import { FormComponent } from './form/form.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    About_meComponent,
+    FormComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
